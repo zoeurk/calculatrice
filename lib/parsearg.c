3855829d1f22d7acd6d3191cb1f89b247a7ab3f0
zoeurk <zoeurk@gmail.com>
2021-05-05 21:35:05 +0200

    update

--- lib/parsearg.c
+++ lib/parsearg.c
@@ -19,6 +19,16 @@ state.err_stream = stderr;\
 memcpy(&state, parser->state, sizeof(struct parser_state));\
 }
 
+int printable(int i){
+	if(
+		/*(i >47 && i < 58) || 
+		(i > 64 && i < 91) ||
+		(i > 96 && i < 124)*/
+		i > 32 && i < 127
+
+	)return 1;
+	return 0;
+}
 void upper(int *len, int inc,int max){
 	if(*len + inc > max){
 		printf("\n\t");
@@ -90,9 +100,7 @@ void parser_usage(struct parser *parser){
 	c++;*/
 	/*n = 0;*/
 	for(i = 0;z = 0, options[i].longoption != 0; i++){
-		if((options[i].shortoption >47 && options[i].shortoption < 58) || 
-			(options[i].shortoption >64 && options[i].shortoption < 91) ||
-			(options[i].shortoption >96 && options[i].shortoption < 124)
+		if(printable(options[i].shortoption) 
 		)	fprintf(state.out_stream," -%c, --", options[i].shortoption);
 		else	fprintf(state.out_stream,"     --");
 		for(j = 0, k = 7,z = 0; j < strlen(options[i].longoption);j++, k++){
@@ -186,37 +194,39 @@ void parser_short_usage(struct parser *parser){
 	}
 	for(i = 0, j = 0; options[i].longoption != 0; i++){
 		if(options[i].flags&OPTION_ARG_OPTIONAL){
-			upper(&k, 8+strlen(options[i].args), l);
-			fprintf(state.out_stream,"[-%c [%s]] ",options[i].shortoption, options[i].args);
-			k += 8+strlen(options[i].args);
+			upper(&k, 10+strlen(options[i].args), l);
+			if(printable(options[i].shortoption))
+				fprintf(state.out_stream,"[-%c [%s]] ",options[i].shortoption, options[i].args);
+			k += 10+strlen(options[i].args);
 		}
 	}
 	for(i = 0, j = 0; options[i].longoption != 0; i++){
 		if((options[i].flags&OPTION_ARG_OPTIONAL) == 0 && options[i].args && options[i].shortoption){
-			upper(&k, 6+strlen(options[i].args),l);
-			fprintf(state.out_stream,"[-%c %s] ",options[i].shortoption, options[i].args);
-			k += 6+strlen(options[i].args);
+			upper(&k, 7+strlen(options[i].args),l);
+			if(printable(options[i].shortoption))
+				fprintf(state.out_stream,"[-%c %s] ",options[i].shortoption, options[i].args);
+			k += 7+strlen(options[i].args);
 		}
 	}
 	for(i = 0;options[i].longoption != 0; i++){
 		if(options[i].args == NULL){
-			upper(&k, 5+strlen(options[i].longoption), l);
+			upper(&k, 6+strlen(options[i].longoption), l);
 			fprintf(state.out_stream,"[--%s] ", options[i].longoption);
 			k += 5+strlen(options[i].longoption);
 		}
 	}
 	for(i = 0, j = 0; options[i].longoption != 0; i++){
 		if(options[i].flags&OPTION_ARG_OPTIONAL){
-			upper(&k, 8+strlen(options[i].longoption) + strlen(options[i].args), l);
+			upper(&k, 10+strlen(options[i].longoption) + strlen(options[i].args), l);
 			fprintf(state.out_stream,"[--%s[=%s]] ",options[i].longoption, options[i].args);
-			k += 8+strlen(options[i].longoption) + strlen(options[i].args);
+			k += 10+strlen(options[i].longoption) + strlen(options[i].args);
 		}
 	}
 	for(i = 0, j = 0; options[i].longoption != 0; i++){
 		if((options[i].flags&OPTION_ARG_OPTIONAL) == 0 && options[i].args){
-			upper(&k, strlen(options[i].longoption) + strlen(options[i].args) +6, l);
+			upper(&k, strlen(options[i].longoption) + strlen(options[i].args) +7, l);
 			fprintf(state.out_stream,"[--%s=%s] ",options[i].longoption, options[i].args);
-			k += strlen(options[i].longoption) + strlen(options[i].args) +6;
+			k += strlen(options[i].longoption) + strlen(options[i].args) +7;
 		}
 	}
 	upper(&k, 9, l);
@@ -234,7 +244,7 @@ void parser_short_usage(struct parser *parser){
 		k += 10;
 	}
 	if(parser->args_doc)
-		fprintf(state.out_stream, "%s\n", parser->args_doc);
+		fprintf(state.out_stream, "\n\t%s\n", parser->args_doc);
 	else
 		fprintf(state.out_stream, "\n");
 }
