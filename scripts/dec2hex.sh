79346d3502c465fd72901d8b57806d3df7e0145d
zoeurk <zoeurk@gmail.com>
2021-06-07 17:30:34 +0200

    update

--- scripts/dec2hex.sh
+++ scripts/dec2hex.sh
@@ -118,8 +118,8 @@ hexadecimal(){
 case $1
 in
 TO_HEX)
-	ENTIER=`./calcule -O 0 "floor($VAL)"`
-	SUB=`./calcule -O 6 "( $VAR - $ENTIER )"`
+	ENTIER=`calcule -O 0 "floor($VAL)"`
+	SUB=`calcule -O 6 "( $VAR - $ENTIER )"`
 	dot(){
 		I=0
 		printf " $VAL" | sed -e 's/\(.\)/\1\n/g' -e 's/\n$//' | \
@@ -135,33 +135,33 @@ TO_HEX)
 	for V in $ENTIER $SUB
 	do
 		VAR=$V
-		while ./mcompare "( $VAR != 0)"
+		while mcompare "( $VAR != 0)"
 		do
 			VALUE=$V
-			if ./mcompare "\-N $VAR"
+			if mcompare "\-N $VAR"
 			then
 				printf "Caractere invalid dans: $VAR\n"
 				exit
 			fi
 			if test $VIRGULE -eq 0
 			then
-				VAL=`./calcule -O 0 "mod($VAR,16)"`
-				VALUE=`./calcule "( $VAR-$VAL )"`
+				VAL=`calcule -O 0 "mod($VAR,16)"`
+				VALUE=`calcule "( $VAR-$VAL )"`
 				VAL=`hexadecimal $VAL`
-				VAR=`./calcule -O 0 "( $VALUE/16 )"`
+				VAR=`calcule -O 0 "( $VALUE/16 )"`
 				RESULT=${VAL}${RESULT}
 			else
 				VIRGULE=$(($VIRGULE+1))
-				VALUE=`./calcule "$VAR*16"`
-				ENTIER=`./calcule -O 0 "floor($VALUE)"`
-				VAR=`./calcule "( $VALUE - $ENTIER )"`
+				VALUE=`calcule "$VAR*16"`
+				ENTIER=`calcule -O 0 "floor($VALUE)"`
+				VAR=`calcule "( $VALUE - $ENTIER )"`
 				RESULT=${RESULT}`hexadecimal $ENTIER`
 				LAST=$RESULT
 				DOT=$(($DOT-1))
 				test $DOT -eq 0 && break
 			fi
 		done
-		if ./mcompare "$VIRGULE == 0 && 0 < $SUB"
+		if mcompare "$VIRGULE == 0 && 0 < $SUB"
 		then
 			VIRGULE=1
 			RESULT="${RESULT}."
@@ -196,7 +196,7 @@ TO_DEC)
 			printf "Caractere invalide: $V\n"
 			exit
 		fi
-		RESULT=`./calcule "$VAL * pow(16,$I) + $RESULT"`
+		RESULT=`calcule "$VAL * pow(16,$I) + $RESULT"`
 		fi
 	done
 	printf "$RESULT\n"
