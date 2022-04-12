2a53b752cc7ead74636bfd0ac87a0c851dd3e6d0
zoeurk <zoeurk@gmail.com>
2022-04-12 14:11:00 +0200

    update

--- ntest/ntest.c
+++ ntest/ntest.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "../lib/parsearg.h"
+#include "parsearg.h"
 
 #include <sys/types.h>
 #include <sys/stat.h>
@@ -792,15 +792,12 @@ struct retour *reader(char *string, unsigned long int type){
 					default:
 						if(o.var1 == NULL){
 							o.var1 = r;
-						}else{
-							fprintf(stderr,"Argument invalide:%c\n", *r);
-							exit(EXIT_FAILURE);
 						}
 						/*if(end == 1){
 							fprintf(stderr, "=>Erreur de syntaxe vers l'offset: %lu.\n", offset);
 							exit(EXIT_FAILURE);
 						}*/
-						if((*r < 48 || *r > 57) && *r != '.' && *r != '-')
+						if((*r < 48 || *r > 57) && *r != '.' && *r != '-' && *r != '+')
 							o.type = STRING;
 						break;
 				}
